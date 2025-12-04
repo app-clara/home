4d0849a16f59cb1da5aca89713d3fb23455e7886
Clara App <claradigital.app@gmail.com>
2025-12-04 01:10:30 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 
 export default defineConfig({
-  base: '/home/', // 👈 importante — nome do repositório
+   //base: '/home/', // 👈 importante — nome do repositório
   plugins: [react()],
   resolve: {
     alias: {
